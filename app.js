c68cf548fe45d8f38771c24a1a1037d30e796526
Maël <maelastier.contact@gmail.com>
2018-09-04 13:47:54 +0200

    Add new way

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
 app.set("view engine", "pug");
 
 app.get("/", function(req, res) {
-  res.render("index", { title: "Hey", message: "Hello there!" });
+  res.render("index", {title: 'nodeapp'});
+});
+
+app.get("/login", function(req, res) {
+  res.render("login", {title: 'nodeapp'});
+});
+
+app.get("/signup", function(req, res) {
+  res.render("signup", { title: "nodeapp"});
 });
 
 app.listen(3000, () =>
